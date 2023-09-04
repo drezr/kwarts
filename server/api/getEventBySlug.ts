78a7336a055f9f49c23aed324bf1000f033c08e6
drezr <ultimove@gmail.com>
2023-09-04 23:44:18 +0200

    Retrieve elo

--- server/api/getEventBySlug.ts
+++ server/api/getEventBySlug.ts
@@ -57,6 +57,7 @@ export default defineEventHandler(async (e: any) => {
                     select: {
                       id: true,
                       alias: true,
+                      elo: true,
                       user: {
                         select: {
                           id: true,
