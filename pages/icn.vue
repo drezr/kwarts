c0ce03ac78e22095adafca8bed606c1d4e10e0e8
drezr <ultimove@gmail.com>
2024-10-19 16:21:26 +0200

    Update icn.vue

--- pages/icn.vue
+++ pages/icn.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="flex justify-center items-center m-1">
-    <input type="number" v-model="targetClubId" class="mr-2 w-24 h-12 rounded" placeholder="Club"
-      v-on:keyup.enter="getData()" />
+    <input
+      type="number"
+      v-model="targetClubId"
+      class="mr-2 w-24 h-12 rounded"
+      placeholder="Club"
+      v-on:keyup.enter="getData()"
+    />
 
-    <input type="number" v-model="targetRound" class="mr-2 w-24 h-12 rounded" placeholder="Round"
-      v-on:keyup.enter="getData()" />
+    <input
+      type="number"
+      v-model="targetRound"
+      class="mr-2 w-24 h-12 rounded"
+      placeholder="Round"
+      v-on:keyup.enter="getData()"
+    />
 
-    <button @click="getData()" class="border border-gray-500 p-3 h-12 mr-2 rounded"
-      :disabled="!targetClubId || !targetRound" :class="{ 'opacity-20': !targetClubId || !targetRound }">
+    <button
+      @click="getData()"
+      class="border border-gray-500 p-3 h-12 mr-2 rounded"
+      :disabled="!targetClubId || !targetRound"
+      :class="{ 'opacity-20': !targetClubId || !targetRound }"
+    >
       GO
     </button>
 
@@ -16,7 +30,9 @@
     </button>
   </div>
 
-  <div id="targetNode" style="
+  <div
+    id="targetNode"
+    style="
       font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
         'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
         'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
@@ -25,38 +41,51 @@
       justify-content: center;
       align-items: center;
       flex-direction: column;
-    ">
+    "
+  >
     <div v-for="division in divisions">
-      <div style="
+      <div
+        style="
           margin-top: 20px;
           font-weight: bold;
           font-size: 22px;
           text-align: center;
-        ">
+        "
+      >
         {{ division.title }}
       </div>
 
-      <div style="
+      <div
+        style="
           border: 1px rgb(156 163 175) solid;
           width: fit-content;
           margin: 10px;
-        ">
-        <div v-for="(result, i) in division.results" style="display: flex"
-          :style="i % 2 ? 'background-color: rgb(241 245 249);' : ''">
+        "
+      >
+        <div
+          v-for="(result, i) in division.results"
+          style="display: flex"
+          :style="i % 2 ? 'background-color: rgb(241 245 249);' : ''"
+        >
           <div style="width: 256px; padding: 4px; text-align: right">
             {{ result.fullname_home }}
-            <small style="color: rgb(107 114 128)">({{ result.rating_home }})</small>
+            <small style="color: rgb(107 114 128)"
+              >({{ result.rating_home }})</small
+            >
           </div>
           <div style="width: 144px; padding: 4px; text-align: center">
             {{ result.result }}
           </div>
           <div style="width: 256px; padding: 4px">
             {{ result.fullname_visit }}
-            <small style="color: rgb(107 114 128)">({{ result.rating_visit }})</small>
+            <small style="color: rgb(107 114 128)"
+              >({{ result.rating_visit }})</small
+            >
           </div>
         </div>
 
-        <div style="
+        <div
+          style="
             display: flex;
             justify-content: center;
             align-items: center;
@@ -64,7 +93,9 @@
             border-top: 1px rgb(156 163 175) solid;
             background-color: rgba(148, 163, 184, 0.4);
             font-weight: bold;
-          " v-if="division.results.length > 0">
+          "
+          v-if="division.results.length > 0"
+        >
           <div>{{ calcResult(division.results) }}</div>
         </div>
       </div>
@@ -99,7 +130,6 @@ async function getData() {
       round: targetRound.value,
     }
   )
-
 
   for (const division of reqDivisions) {
     let targetEncounter = division.rounds[0].encounters.find(
